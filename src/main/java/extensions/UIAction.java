67f7e01e8bae78dcec7a8687649d357e3689d776
moshe <mosr20@gmail.com>
2022-12-04 17:12:40 +0200

    first commit

--- src/main/java/extensions/UIAction.java
+++ src/main/java/extensions/UIAction.java
@@ -14,7 +14,7 @@ public class UIAction extends CommonOps {
 
 
     @Step("clice_on_element")
-    //receives an element and makes a click on element770//
+    //receives an element and makes a click on element//
     public static void click(WebElement elem){
         wait.until(ExpectedConditions.elementToBeClickable(elem));
         elem.click();
